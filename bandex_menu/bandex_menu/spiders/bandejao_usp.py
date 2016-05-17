c7e8adcdc8a5f50e50af698571e99fec6f0dadec
Felipe Besson <flpbesson@gmail.com>
2016-05-17 13:16:49 -0300

    Step 3: pegar lista com urls dos restaurantes

--- bandex_menu/bandex_menu/spiders/bandejao_usp.py
+++ bandex_menu/bandex_menu/spiders/bandejao_usp.py
@@ -7,3 +7,9 @@ from scrapy.contrib.spiders import CrawlSpider
 class BandejaoUspSpider(CrawlSpider):
     name = "bandejao_usp"
     start_urls = ["http://www.usp.br/coseas/COSEASHP/COSEAS2010_cardapio.html"]
+
+    def parse(self, response):
+        sel =  Selector(response)
+        urls_restaurantes = sel.xpath("//h5/a/@href").extract()
+
+        print(urls_restaurantes)
